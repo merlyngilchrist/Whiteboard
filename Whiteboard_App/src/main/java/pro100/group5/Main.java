a0960b1d1aeff4ce48c2dea002a665f83b432154
JaxenSandlandNeumont <jsandland@student.neumont.edu>
2024-07-29 12:08:42 -0600

    Initial commit

---
+++ Whiteboard_App/src/main/java/pro100/group5/Main.java
@@ -0,0 +1,10 @@
+package pro100.group5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
