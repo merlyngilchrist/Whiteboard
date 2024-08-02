0193c67d2bafa500c6b691b37052d8741ec7c09e
JaxenSandlandNeumont <jsandland@student.neumont.edu>
2024-08-02 10:21:39 -0600

    Rebirth

---
+++ src/main/java/backend/pro100/group5/Main.java
@@ -0,0 +1,8 @@
+package backend.pro100.group5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println("Test2");
+    }
+}
