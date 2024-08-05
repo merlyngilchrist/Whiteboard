8a90d70db2a7b52b6eea244371abb70b6efb3867
JaxenSandlandNeumont <jsandland@student.neumont.edu>
2024-08-05 11:47:51 -0600

    Combined ApplicationStarter and WhiteboardController

--- src/main/java/backend/pro100/group5/WhiteboardController.java
+++ src/main/java/backend/pro100/group5/WhiteboardController.java
@@ -1,4 +1,17 @@
 package backend.pro100.group5;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WhiteboardController {
+
+    /**
+     * Automatically gets called by Springboot to use index.html as the displayed html file
+     * @return The name of .html file that should open
+     */
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Connects application to "index.html
+    }
 }
