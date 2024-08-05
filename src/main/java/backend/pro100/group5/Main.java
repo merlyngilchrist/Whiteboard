787d9ea0b8d880d28025bdba2b36c3795c0c9ede
JaxenSandlandNeumont <jsandland@student.neumont.edu>
2024-08-05 11:45:21 -0600

    Combined SpringbootStarter and Main

--- src/main/java/backend/pro100/group5/Main.java
+++ src/main/java/backend/pro100/group5/Main.java
@@ -1,8 +1,16 @@
 package backend.pro100.group5;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
+    /**
+     * Opens Springboot application on port 8080, view it through http://localhost:8080
+     * @param args Arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("Test2");
+        SpringApplication.run(Main.class, args);
     }
 }
