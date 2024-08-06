c5b0eff12bc9c38e9c116e337b454c05f27a1bbc
merlyngilchrist <157403080+merlyngilchrist@users.noreply.github.com>
2024-08-06 11:30:27 -0600

    Updated dependencies and added client communication

--- src/main/java/backend/pro100/group5/WhiteboardController.java
+++ src/main/java/backend/pro100/group5/WhiteboardController.java
@@ -1,7 +1,18 @@
 package backend.pro100.group5;
 
+import com.azure.messaging.webpubsub.WebPubSubServiceClient;
+import com.azure.messaging.webpubsub.models.GetClientAccessTokenOptions;
+import com.azure.messaging.webpubsub.models.WebPubSubClientAccessToken;
+import com.azure.messaging.webpubsub.models.WebPubSubContentType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,7 +21,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@RestController
+@RequestMapping("/api/whiteboard")
 public class WhiteboardController {
+
+    @Autowired
+    private WebPubSubServiceClient webPubSubServiceClient;
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestBody String message){
+        webPubSubServiceClient.sendToAll(message, WebPubSubContentType.TEXT_PLAIN);
+    }
+
+    @GetMapping("/negotiate")
+    public Map<String, String> negotiate(){
+        GetClientAccessTokenOptions options = new GetClientAccessTokenOptions()
+                .setRoles(Collections.singletonList("webpubsub.joinLeaveGroup"));
+        WebPubSubClientAccessToken token = webPubSubServiceClient.getClientAccessToken(options);
+        Map<String, String> response = new HashMap<>();
+        response.put("url", token.getUrl());
+        response.put("accessToken", token.getToken());
+        return response;
+    }
+
+
 
     /**
      * Automatically gets called by Springboot to use index.html as the displayed html file
