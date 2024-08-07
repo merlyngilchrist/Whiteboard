79268c021b82c77428d60f49871af5a627b76c05
VanDameG305 <vandameg305@gmail.com>
2024-08-07 10:40:42 -0600

    I GOT IT DRAWING

--- src/main/resources/static/canvas.js
+++ src/main/resources/static/canvas.js
@@ -1,14 +1,48 @@
-/**
- * Hate sent button has been clicked, calls the "hateButtonClicked" in the WhiteboardController class
+const canvas = document.getElementById("whiteboard");
+if (canvas.getContext) {
+    const context = canvas.getContext("2d");
+    let drawing = false;
+    canvas.addEventListener('mousedown',startDrawing);
+    canvas.addEventListener('mouseup',stopDrawing);
+    canvas.addEventListener('mousemove',draw)
+
+    function startDrawing(event) {
+        drawing = true;
+        draw(event);
+    }
+    function stopDrawing() {
+        drawing = false;
+        context.beginPath();
+    }
+    function draw(event) {
+        if (!drawing) return;
+        context.lineWidth = 5;
+        context.lineCap = 'round';
+        context.strokeStyle = 'black';
+
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        context.lineTo(x,y);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(x,y);
+    }
+}
+/*
+    Sends hate to us lowly developers
  */
 function hateSent() {
-    fetch('/hate-Button-Clicked', {
+    fetch('/hateButtonClicked', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         }
     })
         .then(response => response.json())
-        .then(data => {})
+        .then(data => {
+            console.log(data.message);
+        })
         .catch(error => console.error('Error:', error));
 }
