1b9151168ccc8ffb9c34222976aca229f7b72e3c
merlyngilchrist <157403080+merlyngilchrist@users.noreply.github.com>
2024-08-07 10:21:40 -0600

    new classes

---
+++ src/main/java/backend/pro100/group5/SessionManager.java
@@ -0,0 +1,25 @@
+package backend.pro100.group5;
+
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class SessionManager {
+
+    private static final ConcurrentMap<String , WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    public static void addSession(String sessionId, WebSocketSession session){
+        sessions.put(sessionId,session);
+    }
+
+    public static void removeSession(String sessionId){
+        sessions.remove(sessionId);
+    }
+
+    public static Set<WebSocketSession> getSessions(){
+        return Set.copyOf(sessions.values());
+    }
+
+}
