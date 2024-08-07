573f1f4840626176784bff505d3bdf3f29c7f456
merlyngilchrist <157403080+merlyngilchrist@users.noreply.github.com>
2024-08-07 10:39:41 -0600

    I fixed the conflict

--- src/main/java/backend/pro100/group5/SignalRConfig.java
+++ src/main/java/backend/pro100/group5/SignalRConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import com.azure.messaging.webpubsub.WebPubSubServiceClient;
 import com.azure.messaging.webpubsub.WebPubSubServiceClientBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -14,7 +15,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.io.IOException;
 
 @Component
-public class SignalRConfig extends TextWebSocketHandler {
+public class SignalRConfig extends TextWebSocketHandler{
 
     private static final String CONNECTION_STRING = "Endpoint=https://whiteboard.service.signalr.net;AccessKey=Ydl/+I+T5Q8PJDW8HN5CIQvHVX43UCjy4j1S6kS3d4U=;Version=1.0;";
     private final OkHttpClient client = new OkHttpClient();
