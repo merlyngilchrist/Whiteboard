be4d9542f2599405592bb8cb5c36f10d3ad7a56c
VanDameG305 <vandameg305@gmail.com>
2024-08-09 11:18:31 -0600

    canvas.js Owen Description Comments and Undo/Redo Image renames

--- src/main/resources/static/js/canvas.js
+++ src/main/resources/static/js/canvas.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById("whiteboard");
+
+// This checks if we have a canvas to work with; if we do it executes the code within the if that sets up the canvas resolution and canvas event listeners.
 if (canvas.getContext) {
     const context = canvas.getContext("2d");
     // I got this code from the Mozilla developer documents: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Optimizing_canvas
@@ -15,14 +17,19 @@ if (canvas.getContext) {
     canvas.addEventListener('mouseup',stopDrawing);
     canvas.addEventListener('mousemove',draw)
 
+    // Sets the state of the page to drawing when a mouse is clicked and held down.
     function startDrawing(event) {
         drawing = true;
         draw(event);
     }
+
+    // Once the hold mouse has been released, this makes sure it stops drawing
     function stopDrawing() {
         drawing = false;
         context.beginPath();
     }
+
+    // The default pen settings as well as following the user's mouse when drawing
     function draw(event) {
         if (!drawing) return;
         context.lineWidth = 5;
@@ -38,20 +45,3 @@ if (canvas.getContext) {
         context.moveTo(x,y);
     }
 }
-/*
-    Sends hate to us lowly developers
- */
-function () {
-    fetch('/hateButtonClicked', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message);
-        })
-        .catch(error => console.error('Error:', error));
-}
-
